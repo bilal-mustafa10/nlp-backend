380f8c6d5cb78ac202a49b2df6391827fb368ace
bilal-mustafa10 <bilalmustafa041@icloud.com>
2022-11-17 02:31:16 +0000

    fixed error

--- nlp_backend/__init__.py
+++ nlp_backend/__init__.py
@@ -10,6 +10,6 @@ data_path = "nlp_backend/assets/data.csv"
 df = pd.read_csv(data_path, sep=',', converters={'doc_entities': literal_eval, 'doc_keyphrases': literal_eval})
 interesting = df.drop(['doc_date', 'doc_title', 'doc_entities', 'doc_keyphrases', 'doc_publish_location'], axis=1)
 interesting.dropna()
-interesting['sentence'].fillna('')
+interesting['sentence'] = interesting['sentence'].fillna('')
 model_name = "deepset/roberta-base-squad2"
 fb_ai = pipeline('question-answering', model=model_name, tokenizer=model_name)
